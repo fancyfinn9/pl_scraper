e477829b260ac0163826e8c3dbf5fbb185ec35aa
fancyfinn9 <bence.gergo.zele@gmail.com>
2023-08-30 11:32:29 +0100

    Fix errors in pl_push example

--- EXAMPLES/pl_push.py
+++ EXAMPLES/pl_push.py
@@ -16,7 +16,11 @@ while True:
             data = ""
             
             if event["type"] == "G":
-                title = "GOOOAAL! ("+pl.player(event["personId"])["name"]["display"]+", "+event["clock"]["label"][:-2]+")"
+                player = pl.player(event["personId"])
+                for club in fixture["teams"]:
+                    if club["team"]["id"] == event["teamId"]:
+                        team = club["team"]["club"]["abbr"]
+                title = "GOOOAAL! "+pl.player(event["personId"])["name"]["display"]+" ("+team+") - "+event["clock"]["label"][:-2]
             elif event["type"] == "PS":
                 if event["phase"] == "1": title = "KICKOFF at "+fixture["ground"]["name"]
                 elif event["phase"] == "2": title = "2ND HALF BEGINS at "+fixture["ground"]["name"]
@@ -26,20 +30,28 @@ while True:
                 elif event["phase"] == "2": title = "FULL TIME at "+fixture["ground"]["name"]
                 else: title = "<Unknown Play End event>"
             elif event["type"] == "S":
-                title = "SUB "+event["description"]+" - "+pl.player(event["personId"])["name"]["display"]+" ("+event["clock"]["label"][:-2]+")"
+                player = pl.player(event["personId"])
+                for club in fixture["teams"]:
+                    if club["team"]["id"] == event["teamId"]:
+                        team = club["team"]["club"]["abbr"]
+                title = "SUB "+event["description"]+" - "+pl.player(event["personId"])["name"]["display"]+" ("+team+") - "+event["clock"]["label"][:-2]
             elif event["type"] == "B":
                 try:
                     x = event["personId"]
+                    player = pl.player(event["personId"])
+                    for club in fixture["teams"]:
+                        if club["team"]["id"] == event["teamId"]:
+                            team = club["team"]["club"]["abbr"]
                     if event["description"] == "Y":
-                        title = "YELLOW CARD - "+pl.player(event["personId"])["name"]["display"]+" ("+event["clock"]["label"][:-2]+")"
+                        title = "YELLOW CARD - "+player["name"]["display"]+" ("+team+") - "+event["clock"]["label"][:-2]
                     elif event["description"] == "R":
-                        title = "RED CARD - "+pl.player(event["personId"])["name"]["display"]+" ("+event["clock"]["label"][:-2]+")"
+                        title = "RED CARD - "+player["name"]["display"]+" ("+event["clock"]["label"][:-2]+")"
                 except KeyError:
                     event["personId"] = "<Unknown player>"
                     if event["description"] == "Y":
-                        title = "YELLOW CARD - "+event["personId"]+" ("+event["clock"]["label"][:-2]+")"
+                        title = "YELLOW CARD - "+event["personId"]+" - "+event["clock"]["label"][:-2]
                     elif event["description"] == "R":
-                        title = "RED CARD - "+event["personId"]+" ("+event["clock"]["label"][:-2]+")"
+                        title = "RED CARD - "+event["personId"]+" - "+event["clock"]["label"][:-2]
             else:
                 title = "<Unknown event>"
             data = data+fixture["teams"][0]["team"]["club"]["shortName"]+" v "+fixture["teams"][1]["team"]["club"]["shortName"]
